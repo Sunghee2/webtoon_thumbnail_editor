a6a6fd639feeb3b577e9e600d2bfb5028b4bd97d
Imtae <95imtae@gmail.com>
2020-05-11 21:00:15 +0900

    feat: initFirebase() 추가

---
+++ src/components/Firebase.js
@@ -0,0 +1,22 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDD_AW90cyn1jfsU4TQWmwqrgCHxiOWtJI',
+  authDomain: 'webtoon-thumbnail-editor.firebaseapp.com',
+  databaseURL: 'https://webtoon-thumbnail-editor.firebaseio.com',
+  projectId: 'webtoon-thumbnail-editor',
+  storageBucket: 'webtoon-thumbnail-editor.appspot.com',
+  messagingSenderId: '385711263103',
+  appId: '1:385711263103:web:338c6a98457ecfb644de4a',
+  measurementId: 'G-V0979BKPDQ',
+};
+// let database;
+
+const initFirebase = () => {
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  }
+  // database = firebase.database();
+};
+
+export default initFirebase;
