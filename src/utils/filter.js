a4e351261ad0c085b87981069cfbc526a4d35e01
Sunghee <630sunghee@gmail.com>
2020-05-13 18:47:56 +0900

    feat: contrast 필터 추가

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,9 +1,26 @@
+const truncate = value => {
+  if (value > 255) return 255;
+  if (value < 0) return 0;
+  return value;
+};
+
 export const brightnessFilter = (pixels, value) => {
   const d = pixels.data;
   for (let i = 0; i < d.length; i += 4) {
     d[i] += value / 3;
     d[i + 1] += value / 3;
     d[i + 2] += value / 3;
+  }
+  return pixels;
+};
+
+export const contrastFilter = (pixels, value) => {
+  const d = pixels.data;
+  const factor = (259 * (value + 255)) / (255 * (259 - value));
+  for (let i = 0; i < d.length; i += 4) {
+    d[i] = truncate((d[i] - 128) * factor + 128);
+    d[i + 1] = truncate((d[i + 1] - 128) * factor + 128);
+    d[i + 2] = truncate((d[i + 2] - 128) * factor + 128);
   }
   return pixels;
 };
