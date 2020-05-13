3fc41b05b1ef3ae8da064b952272645f34fcd443
Sunghee <630sunghee@gmail.com>
2020-05-14 02:31:55 +0900

    fix: 기존 이미지 필터 적용

--- src/components/Adjust/AdjustList.js
+++ src/components/Adjust/AdjustList.js
@@ -4,32 +4,39 @@ import PropTypes from 'prop-types';
 import { defaultAdjust } from '../../utils/const';
 import AdjustItem from './AdjustItem';
 import { AdjustContext } from '../../context/adjustContext';
-import { brightnessFilter, grayscaleFilter } from '../../utils/filter';
+import { brightnessFilter, grayscaleFilter, contrastFilter } from '../../utils/filter';
 import AdjustCheckbox from './AdjustCheckbox';
 
-const AdjustList = ({ canvasRef }) => {
+const AdjustList = ({ canvasRef, image }) => {
   const [adjust, dispatch] = useContext(AdjustContext);
 
   useEffect(() => {
-    const canvas = canvasRef.current.getContext('2d');
-    let imgData = canvas.getImageData(0, 0, canvasRef.current.width, canvasRef.current.height);
-    Object.entries(adjust).forEach(([key, value]) => {
-      switch (key) {
-        case 'brightness':
-          imgData = brightnessFilter(imgData, value);
-          break;
-        case 'contrast':
-          break;
-        case 'gray':
-          imgData = grayscaleFilter(imgData, 0.01);
-          break;
-        case 'blur':
-          break;
-        default:
-          break;
-      }
-    });
-    canvas.putImageData(imgData, 0, 0);
+    if (image) {
+      const canvas = canvasRef.current.getContext('2d');
+      const { width, height } = canvasRef.current;
+      canvas.drawImage(image, 0, 0, width, height);
+      let imgData = canvas.getImageData(0, 0, width, height);
+      Object.entries(adjust).forEach(([key, value]) => {
+        if (value !== defaultAdjust[key].default) {
+          switch (key) {
+            case 'brightness':
+              imgData = brightnessFilter(imgData, value);
+              break;
+            case 'contrast':
+              imgData = contrastFilter(imgData, value);
+              break;
+            case 'gray':
+              imgData = grayscaleFilter(imgData);
+              break;
+            case 'blur':
+              break;
+            default:
+              break;
+          }
+        }
+      });
+      canvas.putImageData(imgData, 0, 0);
+    }
   }, [adjust]);
 
   const setValue = useCallback((key, value) => {
@@ -63,6 +70,7 @@ const AdjustList = ({ canvasRef }) => {
 
 AdjustList.propTypes = {
   canvasRef: PropTypes.object.isRequired,
+  image: PropTypes.string.isRequired,
 };
 
 export default AdjustList;
