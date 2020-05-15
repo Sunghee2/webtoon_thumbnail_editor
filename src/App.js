e2c7ed04555f07f2b3858c2cec20842dcc089fd9
Imtae <95imtae@gmail.com>
2020-05-15 17:25:07 +0900

    History 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './styles/reset.scss';
 import './styles/base.scss';
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
+// import Footer from './components/Footer';
+import History from './components/History';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Main />
+      <History />
     </div>
   );
 };
